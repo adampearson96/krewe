34bff0ff71e022291bfda8de56f46c99c94f6a74
Adam Pearson <adam.pearson.1996@gmail.com>
2020-03-24 19:53:36 +0000

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -36,7 +36,7 @@ exports.createPages = async ({ graphql, actions }) => {
       context: {
         // Data passed to context is available
         // in page queries as GraphQL variables.
-        path: node.frontmatter.path,
+        slug: node.frontmatter.path,
       },
     })
   })
