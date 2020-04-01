19a3153380ed4de2f29925ae27ee1106ee4a9a12
Adam Pearson <adam.pearson.1996@gmail.com>
2020-04-02 00:50:36 +0100

    Create success.js

---
+++ src/pages/success.js
@@ -0,0 +1,25 @@
+import React from "react"
+import Layout from "../components/layout"
+
+import "../scss/contact.scss"
+
+export default () => {
+  return (
+    <Layout pageTitle="Success">
+      <div id="page-contact-page">
+        <div className="page-title">
+          <div className="outer-container">
+            <h1>Success!</h1>
+          </div>
+        </div>
+        <div className="content-container">
+          <div className="outer-container">
+            <div className="col-one">
+              <p>Your form has been submitted successfully.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
