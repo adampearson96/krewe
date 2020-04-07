1cbd12143b7232bb2035352d5d8b705500faf3b5
Adam Pearson <adam.pearson.1996@gmail.com>
2020-04-07 20:34:42 +0100

    homepage updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -60,118 +60,124 @@ export default () => {
                   Contact
                 </Button>
               </Tween>
+              <Tween
+                from={{ opacity: 0 }}
+                to={{ opacity: 1 }}
+                duration={1}
+              >
+                <Particles
+                  className="particles"
+                  params={{
+                    "particles": {
+                      "number": {
+                        "value": 80,
+                        "density": {
+                          "enable": true,
+                          "value_area": 800
+                        }
+                      },
+                      "color": {
+                        "value": "#ddd"
+                      },
+                      "shape": {
+                        "type": "circle",
+                        "stroke": {
+                          "width": 0,
+                          "color": "#000000"
+                        },
+                        "polygon": {
+                          "nb_sides": 5
+                        },
+                      },
+                      "opacity": {
+                        "value": 0.8,
+                        "random": false,
+                        "anim": {
+                          "enable": false,
+                          "speed": 1,
+                          "opacity_min": 0.1,
+                          "sync": false
+                        }
+                      },
+                      "size": {
+                        "value": 3,
+                        "random": true,
+                        "anim": {
+                          "enable": false,
+                          "speed": 40,
+                          "size_min": 0.1,
+                          "sync": false
+                        }
+                      },
+                      "line_linked": {
+                        "enable": true,
+                        "distance": 150,
+                        "color": {
+                          "value": "#ddd",
+                        },
+                        "opacity": 1,
+                        "width": 1
+                      },
+                      "move": {
+                        "enable": true,
+                        "speed": 6,
+                        "direction": "none",
+                        "random": false,
+                        "straight": false,
+                        "out_mode": "out",
+                        "bounce": false,
+                        "attract": {
+                          "enable": false,
+                          "rotateX": 600,
+                          "rotateY": 1200
+                        }
+                      }
+                    },
+                    "interactivity": {
+                      "detect_on": "canvas",
+                      "events": {
+                        "onhover": {
+                          "enable": true,
+                          "mode": "repulse"
+                        },
+                        "onclick": {
+                          "enable": true,
+                          "mode": "push"
+                        },
+                        "resize": true
+                      },
+                      "modes": {
+                        "grab": {
+                          "distance": 400,
+                          "line_linked": {
+                            "opacity": 1
+                          }
+                        },
+                        "bubble": {
+                          "distance": 400,
+                          "size": 40,
+                          "duration": 2,
+                          "opacity": 8,
+                          "speed": 3
+                        },
+                        "repulse": {
+                          "distance": 200,
+                          "duration": 0.4
+                        },
+                        "push": {
+                          "particles_nb": 4
+                        },
+                        "remove": {
+                          "particles_nb": 2
+                        }
+                      }
+                    },
+                    "retina_detect": true
+                  }} 
+                />
+              </Tween>
             </Timeline>
           </div>
-          <Particles 
-            className="particles"
-            params={{
-              "particles": {
-                "number": {
-                  "value": 80,
-                  "density": {
-                    "enable": true,
-                    "value_area": 800
-                  }
-                },
-                "color": {
-                  "value": "#ddd"
-                },
-                "shape": {
-                  "type": "circle",
-                  "stroke": {
-                    "width": 0,
-                    "color": "#000000"
-                  },
-                  "polygon": {
-                    "nb_sides": 5
-                  },
-                },
-                "opacity": {
-                  "value": 0.8,
-                  "random": false,
-                  "anim": {
-                    "enable": false,
-                    "speed": 1,
-                    "opacity_min": 0.1,
-                    "sync": false
-                  }
-                },
-                "size": {
-                  "value": 3,
-                  "random": true,
-                  "anim": {
-                    "enable": false,
-                    "speed": 40,
-                    "size_min": 0.1,
-                    "sync": false
-                  }
-                },
-                "line_linked": {
-                  "enable": true,
-                  "distance": 150,
-                  "color": {
-                    "value": "#ddd",
-                  },
-                  "opacity": 1,
-                  "width": 1
-                },
-                "move": {
-                  "enable": true,
-                  "speed": 6,
-                  "direction": "none",
-                  "random": false,
-                  "straight": false,
-                  "out_mode": "out",
-                  "bounce": false,
-                  "attract": {
-                    "enable": false,
-                    "rotateX": 600,
-                    "rotateY": 1200
-                  }
-                }
-              },
-              "interactivity": {
-                "detect_on": "canvas",
-                "events": {
-                  "onhover": {
-                    "enable": true,
-                    "mode": "repulse"
-                  },
-                  "onclick": {
-                    "enable": true,
-                    "mode": "push"
-                  },
-                  "resize": true
-                },
-                "modes": {
-                  "grab": {
-                    "distance": 400,
-                    "line_linked": {
-                      "opacity": 1
-                    }
-                  },
-                  "bubble": {
-                    "distance": 400,
-                    "size": 40,
-                    "duration": 2,
-                    "opacity": 8,
-                    "speed": 3
-                  },
-                  "repulse": {
-                    "distance": 200,
-                    "duration": 0.4
-                  },
-                  "push": {
-                    "particles_nb": 4
-                  },
-                  "remove": {
-                    "particles_nb": 2
-                  }
-                }
-              },
-              "retina_detect": true
-            }} 
-          />
         </section>
       </BodyClassName>
     </Layout>
